7065d862360a56c3568d743761ddddb1b30c8a72
biningo <biningo@yeah.net>
2020-08-07 20:11:56 +0800

    add:时间的下拉菜单

---
+++ src/components/Menu/MenuItem/TimeMenu.tsx
@@ -0,0 +1,25 @@
+import {TimeProps} from "../../../common/interfaces/TimeProps";
+import {Menu} from "antd";
+import React from "react";
+
+const TimeMenu = (props:TimeProps)=>{
+    const {Time,SetTime,Times} = props;
+    const changeTime = (e: any) => {
+        SetTime(e.key);
+    };
+
+    return (
+        <Menu theme="dark" onClick={changeTime}>
+            {Times.map(time => {
+                return (
+                    <Menu.Item key={time}>
+                        {time}
+                    </Menu.Item>
+
+                )
+            })}
+        </Menu>
+    )
+};
+
+export default TimeMenu;
