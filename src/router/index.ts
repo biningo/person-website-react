fff26cad950814b60e11ef6de093f159eff29031
biningo <biningo@yeah.net>
2020-08-10 16:35:40 +0800

    增加archive路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ const Home = React.lazy(() => import("../pages/home/home"));
 const Album = React.lazy(()=>import("../pages/album/album"));
 const About = React.lazy(()=>import("../pages/about/about"));
 const Blog = React.lazy(()=>import("../pages/blog/blog"));
-
+const Archive = React.lazy(()=>import("../pages/archive/archive"));
 
 export interface IRoute {
     path: string,
@@ -32,6 +32,10 @@ const routers: IRoute[] = [
     {
         path:'/blog',
         component:Blog
+    },
+    {
+        path:'/archive',
+        component:Archive
     }
 ];
 
